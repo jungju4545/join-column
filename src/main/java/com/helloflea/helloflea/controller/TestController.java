ff3b0610539b8d46528efe4c23bf4ce110d2e027
Lee <jwlee1024@gmail.com>
2019-12-15 22:17:45 +0900

    index login join 양식 프로토타입

--- src/main/java/com/helloflea/helloflea/controller/TestController.java
+++ src/main/java/com/helloflea/helloflea/controller/TestController.java
@@ -20,4 +20,19 @@ public class TestController {
 	public @ResponseBody String test() {
 		return "<h1>test</h1>";
 	}
+	
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/join")
+	public String join() {
+		return "join";
+	}
 }
